6cd0885f4090fb53a6d476100f5031093efd57c5
PiumeeSene <sene0029@flinders.edu.au>
2025-04-05 00:10:06 +1030

    extra dto and object mapping files added for venues

---
+++ Fringe/Fringe.Domain/DTOs/VenueDTOs/CreateVenueDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Fringe.Domain.DTOs;
+
+public class CreateVenueDto
+{
+    public string VenueName { get; set; }
+    public int TypeId { get; set; }
+    public int MaxCapacity { get; set; }
+    public string Description { get; set; }
+    public string ContactEmail { get; set; }
+    public string ContactPhone { get; set; }
+    public bool IsAccessible { get; set; }
+    public string VenueUrl { get; set; }
+    public int LocationId { get; set; }
+}
