0eaf881d99654aa0593e61069beedd2e9bd0b8d2
PiumeeSene <sene0029@flinders.edu.au>
2025-04-05 21:38:10 +1030

    Location CRUD APIs implemented

---
+++ Fringe/Fringe.Domain/DTOs/LocationDTOs/LocationDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Fringe.Domain.DTOs.LocationDTOs;
+
+public class LocationDto
+{
+    public int LocationId { get; set; }
+    public string LocationName { get; set; }
+    public string Address { get; set; }
+    public string Suburb { get; set; }
+    public string PostalCode { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public bool ParkingAvailable { get; set; }
+    public bool Active { get; set; }
+}
